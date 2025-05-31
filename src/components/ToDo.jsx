8bbc9166848c0f51ac7f3b9bb5e1ab377ebdd92f
prajita11 <timprajita100@gmail.com>
2025-05-31 14:47:09 +0545

    get api

--- src/components/ToDo.jsx
+++ src/components/ToDo.jsx
@@ -1,60 +1,71 @@
+// src/components/ToDo.jsx
+
 import React, { useEffect, useRef, useState } from "react";
+import axios from "axios";
 import todo_icon from "../assets/todo_icon.png";
 import ToDoItems from "./ToDoItems";
 
 const ToDo = () => {
-  const [todolist, setTodoList] = useState(localStorage.getItem("todos")? JSON.parse(localStorage.getItem("todos")) :[]);
+  const [todolist, setTodoList] = useState([])
+   
 
   const inputRef = useRef();
 
   const add = () => {
     const inputText = inputRef.current.value.trim();
-
-    if (inputText === "") {
-      return null;
-    }
+    if (!inputText) return;
 
     const newTodo = {
       id: Date.now(),
       text: inputText,
       isComplete: false,
     };
+
     setTodoList((prev) => [...prev, newTodo]);
     inputRef.current.value = "";
-  }
+  };
 
-  const deleteToDo = (id)=>{
-    setTodoList((prvTodos)=>{
-     return prvTodos.filter((todo) =>todo.id !==id)
-    })
-  }
+  const deleteToDo = (id) => {
+    setTodoList((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
 
-  const toggle = (id)=>{
-    setTodoList((prevTodos) =>{
-      return prevTodos.map((todo)=>{
-        if(todo.id === id){
-          return {...todo, isComplete: !todo.isComplete}
-        }
-        return todo;
+  const toggle = (id) => {
+    setTodoList((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, isComplete: !todo.isComplete } : todo
+      )
+    );
+  };
+
+  // Fetch todos on first load
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then((res) => {
+        const formatted = res.data.map((item) => ({
+          id: item.id,
+          text: item.title,
+          isComplete: item.completed,
+        }));
+
+
+        const offset = 10000;
+        
+        setTodoList(() => formatted);
       })
-    })
-  }
+      .catch((err) => console.error("Fetch error:", err));
+  }, []);
 
-  useEffect(()=>{
-    localStorage.setItem("todos", JSON.stringify(todolist))
-  },[todolist])
-
+  
   return (
-    <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl ">
-      {/* -------------title ---------*/}
-
+    <div className="bg-white place-self-center w-11/12 max-w-md flex flex-col p-7 min-h-[550px] rounded-xl">
+      {/* Title */}
       <div className="flex items-center mt-7 gap-2">
-        <img className="w-8" src={todo_icon} alt="" />
+        <img className="w-8" src={todo_icon} alt="Todo Icon" />
         <h1 className="text-3xl font-semibold">To-Do List</h1>
       </div>
 
-      {/* -------------input box ---------*/}
-
+      {/* Input Box */}
       <div className="flex items-center my-7 bg-gray-200 rounded-full">
         <input
           ref={inputRef}
@@ -64,27 +75,24 @@ const ToDo = () => {
         />
         <button
           onClick={add}
-          className="borded-none rounded-full bg-orange-600 w-32 h-14 text-white text-large font-medium cursor-pointer"
+          className="borded-none rounded-full bg-green-600 w-32 h-14 text-white text-large font-medium cursor-pointer"
         >
           ADD +
         </button>
       </div>
 
-      {/* -------------todo list ---------*/}
-
+      {/* All Todos */}
       <div>
-        {todolist.map((item, index) => {
-          return (
-            <ToDoItems
-              key={index}
-              text={item.text}
-              id={item.id}
-              isComplete={item.isComplete}
-              deleteToDo = {deleteToDo}
-              toggle ={toggle}
-            />
-          );
-        })}
+        {todolist.map((item) => (
+          <ToDoItems
+            key={item.id}
+            id={item.id}
+            text={item.text}
+            isComplete={item.isComplete}
+            deleteToDo={deleteToDo}
+            toggle={toggle}
+          />
+        ))}
       </div>
     </div>
   );
